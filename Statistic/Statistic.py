36d923692a886fa07db3f4e72ff5f5f8c71b17ed
saltowl <vladislava-shakirova@mail.ru>
2018-05-22 13:01:20 +0300

    Complete third task: add 2 new tables, Pearson test

--- Statistic/Statistic.py
+++ Statistic/Statistic.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+from scipy.stats import norm
 
 countInterval = 12
 t = 1.95
 
 def PrintHistogram(x, y, step):
     plt.style.use("bmh")
-    plt.bar(x, y, step - 0.05, align = 'edge', color = 'sandybrown', edgecolor = 'sienna')
+    plt.bar(x, y, step, align = 'edge', color = 'sandybrown', edgecolor = 'sienna', linewidth = 1.5)
     plt.xlabel("x")
     plt.ylabel("p / h")
     plt.title("Histogram")
@@ -54,6 +55,13 @@ def TableHeader():
     print("|\t\t|   Borders of  | Middle of | Count of numbers |\t     |         |         |")
     print("| № of interval |    interval   |  interval |    on interval   | Probability |  xᵢ*pᵢ  |  xᵢ²*pᵢ |")
     print("|_______________|_______________|___________|__________________|_____________|_________|_________|")
+
+def TableHeaderNew():
+    print("\nTable 2")
+    print("_____________________________________________________")
+    print("|   Borders of  |        |        |        |        |")
+    print("|    interval   |   Zᵢ   |  F(Zᵢ) |   pᵢ   |   nᵢ   |")
+    print("|_______________|________|________|________|________|")
 
 
 def Table(begin, data, end, step, minNum, maxNum):
@@ -110,6 +118,7 @@ def Table(begin, data, end, step, minNum, maxNum):
     xGraph.append(float(end))
     Fn.extend([1.0] * 2)
     PrintGraph(Fn, xGraph, begin - 1, end + 1)
+    return (expectedValue, dispersion)
 
 
 def PrintData(data):
@@ -121,6 +130,118 @@ def PrintData(data):
             print("{:6} {:6} {:6} {:6} {:6} {:6} {:6} {:6} {:6} {:6} ".format(*data[x:x+10]))
             x += 10
         print()
+
+def PearsonDistribution(begin, data, end, step, expectedValue, dispersion):
+    i = 0
+    xi = begin
+    newCount = 0
+    probabilityGroup = 0
+    nGroup = 0
+    npiGroup = 0
+    itemForRearsonGroup = 0
+    nGroupList = []
+    probabilityGroupList = []
+
+    TableHeaderNew()
+    
+    while i < countInterval:
+        countNumOnInterval = 0
+        zi = 0
+        F = 0
+        probability = 0
+        npi = 0
+        itemForPearson = 0
+        
+        if i != countInterval - 1:
+            zi = ((xi + step) - expectedValue) / math.sqrt(dispersion)
+        else:
+            zi = math.inf
+
+        F = norm.cdf(zi)
+
+        if i != 0:
+            probability = F - buf
+        else:
+            probability = F
+
+        for item in data:
+            if (((item < xi + step) & (item > xi) & (xi != begin) & (xi != end)) 
+                | ((xi == begin) & (item < xi + step)) 
+                | ((xi == end) & (item > xi))):
+                countNumOnInterval +=1
+        
+        if (countNumOnInterval >= 5):
+            newCount += 1
+            probabilityGroupList.append(probability)
+            nGroupList.append(countNumOnInterval)
+        else:
+            probabilityGroup += probability
+            nGroup += countNumOnInterval
+            if nGroup >= 5:
+                nGroupList.append(nGroup)
+                probabilityGroupList.append(probabilityGroup)
+                nGroup = 0
+                probabilityGroup = 0
+            elif i == countInterval - 1:
+                nGroupList.append(nGroupList.pop() + nGroup)
+                probabilityGroupList.append(probabilityGroupList.pop() + probabilityGroup)
+        
+        npi = probability * len(data)
+        itemForPearson = countNumOnInterval**2 / npi
+
+        if (i > 0 and i < countInterval - 1):
+            print('| ({0:5};{1:5}) | {2:6} | {3:^6} | {4:^6} | {5:^6} |'
+              .format(xi, xi + step, round(zi, 3), round(F, 3), round(probability, 3), countNumOnInterval))
+        elif (i == 0):
+            print('| (  -∞ ;{1:5}) | {2:6} | {3:^6} | {4:^6} | {5:^6} |'
+              .format(xi, xi + step, round(zi, 3), round(F, 3), round(probability, 3), countNumOnInterval))
+        elif (i == countInterval - 1):
+            print('| ({0:5};  +∞ ) |   +∞   | {1:^6} | {2:^6} | {3:^6} |'
+              .format(xi, round(F, 3), round(probability, 3), countNumOnInterval))
+        
+        i += 1
+        xi += step
+        buf = F
+
+    print("————————————————————————————————————————————————————")
+    PrintGroupTable(nGroupList, probabilityGroupList, newCount)
+  
+
+def PrintGroupTable(nGroupList, probabilityGroupList, newCount):
+    print('\n\nResults of grouping in {0} intervals'.format(newCount))
+    print('\nProbability:')
+    print([ '%.2f' % elem for elem in probabilityGroupList ])
+
+    print('\nCount of numbers on interval:')
+    print(nGroupList)
+
+    print('\nnᵢ²')
+    i = 0
+    while i <= newCount:
+        nGroupList[i] = nGroupList[i]**2
+        i += 1
+    print(nGroupList)
+
+    print('\nnpᵢ')
+    i = 0
+    while i <= newCount:
+        probabilityGroupList[i] *= 200
+        i += 1
+    print([ '%.2f' % elem for elem in probabilityGroupList ])
+
+    print('\nnᵢ²/npᵢ')
+    i = 0
+    sum = 0
+    while i <= newCount:
+        nGroupList[i] /= probabilityGroupList[i]
+        sum += nGroupList[i]
+        i += 1
+    print([ '%.2f' % elem for elem in nGroupList ])
+
+    print('\nSum (nᵢ²/npᵢ) = {0:5}'.format(round(sum, 3)))
+    print('\nPearson test = {0:5}'.format(round(sum - 200, 3)))
+
+
 
 def Main():
     input = open("data.txt")
@@ -138,7 +259,8 @@ def Main():
 
     step = round((maxNum - minNum) / countInterval, 1)
 
-    Table(begin, data, end, step, minNum, maxNum)
+    (expectedValue, dispersion) = Table(begin, data, end, step, minNum, maxNum)
+    PearsonDistribution(begin, data, end, step, expectedValue, dispersion)
 
 
 Main()
